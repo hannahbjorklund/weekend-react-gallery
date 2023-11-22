f17ff000c25380b8ff937e673aa48ff85473c822
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-22 09:38:05 -0600

    Did some setup, added more goat pics, updated database.sql with the new goat pics

--- database.sql
+++ database.sql
@@ -10,4 +10,7 @@ INSERT INTO "gallery"
   ("url", "title", "description")
   VALUES
   ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
+  ('images/goat-baby.jpeg', 'Baby Goat!', 'Photo of a little baby goat standing on a stump.');
+  ('images/goat-big.jpeg', 'Senor Goat!', 'Photo of a very stately old goat.');
+  ('images/goat-silly.jpeg', 'Look at this Goat!', 'A cute goat sticks its tongue out in a silly fashion.');
   
