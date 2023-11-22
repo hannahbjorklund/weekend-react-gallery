1d0ef13b1e65343782d3a6a3a6a69c16278e1f54
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-22 16:21:47 -0600

    Made PUT and GET routes in the router file

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg')
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  const sqlQueryText = `
+    UPDATE "gallery"
+      SET "like" = "like"+1
+      WHERE "id" = $1;`;
+  
+  const sqlValues = [req.params.id];
+
+  pool.query(sqlQueryText, sqlValues)
+  .then((result) => {
+    console.log("PUT request at /gallery/like/:id");
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log("Error in PUT:", error);
+    res.sendStatus(500);
+  })
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlQueryText = `
+  SELECT * FROM "gallery";`;
+
+  pool.query(sqlQueryText)
+  .then((result) => {
+    console.log("GET request at /gallery");
+    res.send(result.rows)
+  }).catch((error) => {
+    console.log("Error in GET:", error);
+  })
 });
 
 module.exports = router;
